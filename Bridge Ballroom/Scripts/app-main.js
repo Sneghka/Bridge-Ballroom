4088b545672cf75c16a1211397cd808630d77837
Sneghka <sneghkan@mail.ru>
2017-02-10 10:48:36 +0200

    add script

--- Bridge Ballroom/Scripts/app-main.js
+++ Bridge Ballroom/Scripts/app-main.js
@@ -7,27 +7,29 @@
         var confirmPassword = $("#confirm-password");
 
         if (user.val() === "") {
-           
             $(".error-message1").css("visibility", "visible");
-            
+            $(".register-form div:first-child input").addClass("invalid-box");
         }
         if (password.val() === "") {
-
             $(".error-message2").css("visibility", "visible");
-           
+            $(".register-form div:nth(1) input").addClass("invalid-box");
         }
         if (password.val() !== confirmPassword.val()) {
-
             $(".error-message3").css("visibility", "visible");
-        }
+            $(".register-form div:nth(2) input").addClass("invalid-box");        }
 
       if (isFormValid(user, password, confirmPassword) == false) {
-            alert(user.val());
-            alert(password.val());
-            alert(confirmPassword.val());
-            e.preventDefault();
+           e.preventDefault();
         }
     });
+    $("input").keyup(function () {
+       if ($(this).val() !== "") {
+            $(this).removeClass("invalid-box");
+            $(this).next().css("visibility", "hidden");
+        }
+    });
+
+
 });
 
 function isFormValid(user, password, confirmPassword) {
